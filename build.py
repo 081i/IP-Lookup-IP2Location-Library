7c58cb0000c15e26255598f5ab6b6663748427fb
081i <130923455+081i@users.noreply.github.com>
2023-07-02 01:39:48 +0200

    Update main.py

--- build.py
+++ build.py
@@ -1,64 +1,102 @@
 import os , colorama , msvcrt
 import IP2Location , folium , time
-
 from colorama import Fore , Style
+from fade import fire
 
 text = """
-  ___   ____    _              __     __  _____ 
- |_ _| |  _ \  | |             \ \   / / |___ / 
-  | |  | |_) | | |      _____   \ \ / /    |_ \ 
-  | |  |  __/  | |___  |_____|   \ V /    ___) |
- |___| |_|     |_____|            \_/    |____/ 
+
+✷ 　 　　 　 ·
+ 　 ˚ * .
+ 　 　　 *　　 * ⋆ 　 .
+ · 　　 ⋆ 　　　 ˚ ˚ 　　 ✦
+ 　 ⋆ · 　 *
+ 　　　　 ⋆ ✧　 　 · 　 ✧　✵
+ 　 · ✵
+ _  ___  _         _ _   __  
+| || . \| |   ___ | | | /. | 
+| ||  _/| |_ |___|| ' |/_  .|
+|_||_|  |___|     |__/   |_| 
+
 """
 
-# Using fade Lib 
-def b(text):
-    system(""); faded = ""
-    red = 0
-    for line in text.splitlines():
-        faded += (f"\033[38;2;{red};255;0m{line}\033[0m\n")
-        if not red > 200:
-            red += 30
-    return faded
-
-TextF = b(text)
+TextF = fire(text)
 print(TextF)
 
-filename = input(Fore.BLUE + '[INFO]' + Fore.WHITE + ':' + Fore.YELLOW + 'Enter The Path To The IP2Location .BIN file | ' + Fore.WHITE)
+inputuser = Fore.RED + "[" + Fore.WHITE + ">>>" + Fore.RED + "]"
+colon = Fore.WHITE + ':'
+colon2 =  Fore.WHITE + ': '
+yellow = Fore.YELLOW
+white = Fore.WHITE
+equals = Fore.WHITE + " == "
+red = Fore.RED
+wall = white + " | "
+t = time.sleep(0.3)
+
+filename = input(inputuser + colon + red + 'Enter The Path To The IP2Location .BIN file' + wall + yellow)
 database = IP2Location.IP2Location(os.path.join('data', filename))
-rec = database.get_all(input(Fore.BLUE + "[INFO]" + Fore.WHITE + ':' + Fore.YELLOW + 'Enter IP | ' + Fore.WHITE))
-time.sleep(2)
-
-print(Fore.GREEN + "-----")
-print(Fore.BLUE + '[GAINED]' + Fore.WHITE + ':' + Fore.GREEN + rec.country_short + Fore.BLUE + ' -- [COUNTRY_SHORT]')
-print(Fore.BLUE + '[GAINED]' + Fore.WHITE + ':' + Fore.GREEN + rec.country_long + Fore.BLUE + ' -- [COUNTRY_LONG]')
-print(Fore.BLUE + '[GAINED]' + Fore.WHITE + ':' + Fore.GREEN + rec.region + Fore.BLUE + ' -- [REGION]')
-print(Fore.BLUE + '[GAINED]' + Fore.WHITE + ':' + Fore.GREEN + rec.city + Fore.BLUE + ' -- [CITY]')
-print(Fore.BLUE + '[GAINED]' + Fore.WHITE + ':' + Fore.GREEN + rec.isp + Fore.BLUE + ' -- [ISP]')
-print(Fore.BLUE + '[GAINED]' + Fore.WHITE + ':' + Fore.GREEN + rec.latitude + Fore.BLUE + ' -- [LAT]')
-print(Fore.BLUE + '[GAINED]' + Fore.WHITE + ':' + Fore.GREEN + rec.longitude + Fore.BLUE + ' -- [LNG]')
-print(Fore.BLUE + '[GAINED]' + Fore.WHITE + ':' + Fore.GREEN + rec.domain + Fore.BLUE + ' -- [DOMAIN]')
-print(Fore.BLUE + '[GAINED]' + Fore.WHITE + ':' + Fore.GREEN + rec.zipcode + Fore.BLUE + ' -- [ZIPCODE]')
-print(Fore.BLUE + '[GAINED]' + Fore.WHITE + ':' + Fore.GREEN + rec.timezone + Fore.BLUE + ' -- [TIMEZONE]')
-print(Fore.BLUE + '[GAINED]' + Fore.WHITE + ':' + Fore.GREEN + rec.netspeed + Fore.BLUE + ' -- [NETSPEED]')
-print(Fore.BLUE + '[GAINED]' + Fore.WHITE + ':' + Fore.GREEN + rec.idd_code + Fore.BLUE + ' -- [IDD_CODE]')
-print(Fore.BLUE + '[GAINED]' + Fore.WHITE + ':' + Fore.GREEN + rec.area_code + Fore.BLUE + ' -- [AREA_CDOE]')
-print(Fore.BLUE + '[GAINED]' + Fore.WHITE + ':' + Fore.GREEN + rec.weather_code + Fore.BLUE + ' -- [WEATHER_CODE]')
-print(Fore.BLUE + '[GAINED]' + Fore.WHITE + ':' + Fore.GREEN + rec.weather_name + Fore.BLUE + ' -- [WEATHER_NAME]')
-print(Fore.BLUE + '[GAINED]' + Fore.WHITE + ':' + Fore.GREEN + rec.mcc + Fore.BLUE + ' -- [MCC]')
-print(Fore.BLUE + '[GAINED]' + Fore.WHITE + ':' + Fore.GREEN + rec.mnc + Fore.BLUE + ' -- [MNC]')
-print(Fore.BLUE + '[GAINED]' + Fore.WHITE + ':' + Fore.GREEN + rec.mobile_brand + Fore.BLUE + '[ -- MOBILE_BRAND]')
-print(Fore.BLUE + '[GAINED]' + Fore.WHITE + ':' + Fore.GREEN + rec.elevation + Fore.BLUE + ' -- [ELEVATION]')
-print(Fore.BLUE + '[GAINED]' + Fore.WHITE + ':' + Fore.GREEN + rec.usage_type + Fore.BLUE + ' -- [USAGE_TYPE]')
-print(Fore.BLUE + '[GAINED]' + Fore.WHITE + ':' + Fore.GREEN + rec.address_type + Fore.BLUE + ' -- [ADDRESS_TYPE]')
-print(Fore.BLUE + '[GAINED]' + Fore.WHITE + ':' + Fore.GREEN + rec.category + Fore.BLUE + ' -- [CATEGORY]')
-print(Fore.GREEN + "-----")
-
-print(Fore.BLUE + '[SYSTEM]' + Fore.WHITE + ':' + Fore.LIGHTBLUE_EX + 'Generating IP Map File')
-print(Fore.BLUE + '[INFO]' + Fore.WHITE + ':' + Fore.LIGHTYELLOW_EX + 'IF YOU HAVE ERROR UPGRADE DATA FILE')
+rec = database.get_all(input(inputuser + colon + red + 'Enter IP ' + wall + yellow))
 time.sleep(3)
+
+programOutput = inputuser = Fore.RED + "[" + Fore.WHITE + ">" + Fore.RED + "]"
+
+
+b1 = red + "["
+b2 = red + "]"
+
+print(white + "----------------------------------------------------------")
+print(programOutput + colon2 + red + rec.country_short + equals + b1 + white + "COUNTRY_SHORT" + b2)
+time.sleep(0.3)
+print(programOutput + colon2 + red + rec.country_long + equals + b1 + white + "COUNTRY_LONG" + b2)
+time.sleep(0.3)
+print(programOutput + colon2 + red + rec.region + equals + b1 + white + "REGION" + b2)
+time.sleep(0.3)
+print(programOutput + colon2 + red + rec.city + equals + b1 + white + "CITY" + b2)
+time.sleep(0.3)
+print(programOutput + colon2 + red + rec.isp + equals + b1 + white + "ISP" + b2)
+time.sleep(0.3)
+print(programOutput + colon2 + red + rec.latitude + equals + b1 + white + "LAT" + b2)
+time.sleep(0.3)
+print(programOutput + colon2 + red + rec.longitude + equals + b1 + white + "LNG" + b2)
+time.sleep(0.3)
+print(programOutput + colon2 + red + rec.domain + equals + b1 + white + "DOMAIN" + b2 )
+time.sleep(0.3)
+print(programOutput + colon2 + red + rec.zipcode + equals + b1 + white + "ZIPCODE" + b2)
+time.sleep(0.3)
+print(programOutput + colon2 + red + rec.timezone + equals + b1 + white + "TIMEZONE" + b2)
+time.sleep(0.3)
+print(programOutput + colon2 + red + rec.netspeed + equals + b1 + white + "NETSPEED" + b2)
+time.sleep(0.3)
+print(programOutput + colon2 + red + rec.idd_code + equals + b1 + white + "IDD_CODE" + b2)
+time.sleep(0.3)
+print(programOutput + colon2 + red + rec.area_code + equals + b1 + white + "AREA_CODE" + b2)
+time.sleep(0.3)
+print(programOutput + colon2 + red + rec.weather_code + equals + b1 + white + "WEATHER_CODE" + b2)
+time.sleep(0.3)
+print(programOutput + colon2 + red + rec.weather_name + equals + b1 + white + "WEATHER_NAME" + b2)
+time.sleep(0.3)
+print(programOutput + colon2 + red + rec.mcc + equals + b1 + white + "MCC" + b2)
+time.sleep(0.3)
+print(programOutput + colon2 + red + rec.mnc + equals + b1 + white + "MNC" + b2)
+time.sleep(0.3)
+print(programOutput + colon2 + red + rec.mobile_brand + equals +  b1 + white + "MOBILE_BRAND" + b2)
+time.sleep(0.3)
+print(programOutput + colon2 + red + rec.elevation + equals + b1 + white + "ELEVATION" + b2)
+time.sleep(0.3)
+print(programOutput + colon2 + red + rec.usage_type + equals + b1 + white + "USAGE_TYPE" + b2)
+time.sleep(0.3)
+print(programOutput + colon2 + red + rec.address_type + equals + b1 + white + "ADDRESS_TYPE" + b2)
+time.sleep(0.3)
+print(programOutput + colon2 + red + rec.category + equals + b1 + white + "CATEGORY" + b2)
+print(white + "----------------------------------------------------------")
+
+foliumOutput = Fore.RED + "[" + Fore.WHITE + "FOLIUM" + Fore.RED + "]"
+warn = Fore.RED + "[" + Fore.WHITE + "WARNING" + Fore.RED + "]"
+
+print(foliumOutput + colon + red + "GENERATING MAP")
+time.sleep(1.5)
+print(warn + colon + red + "IF ERROR UPGRADE TO LAT & LNG")
+time.sleep(6)
 filename = "map.html"
-count = 1
 
 count = 1
 while os.path.isfile(f"map{count}.html"):
@@ -73,11 +111,13 @@ location = rec.country_long
 myMap = folium.Map(location=[lat, lng], zoom_start=9)
 folium.Marker([lat,lng],popup = location).add_to(myMap)
 myMap.save(f'map{count}.html')
-print(Fore.BLUE + '[SYSTEM]' + Fore.WHITE + ':' + Fore.GREEN + 'Generated!')
+print(foliumOutput + colon + red + 'GENERATED')
 time.sleep(1.3)
 filename = f'map{count}.html'
 path = os.path.abspath(filename)
-print(Fore.BLUE + '[INFO]' + Fore.WHITE + ':' + Fore.GREEN + 'Your Map File Is Saved At | ' + Fore.WHITE + f"{path}")
+time.sleep(3)
+print(foliumOutput + colon + red + ' MAP PATH' + wall + red + f"{path}")
+print(white + "----------------------------------------------------------")
 time.sleep(1.5)
-print(Fore.WHITE + "Press Any Key To Exit...")
+print(programOutput + colon + red + "TYPE TO EXIT")
 msvcrt.getch()
